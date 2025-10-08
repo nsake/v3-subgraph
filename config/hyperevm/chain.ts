a471775f7fbccefadfa808e19cd0787ed88f20e7
nsake <brukxkle@gmail.com>
2025-10-08 18:37:10 +0300

    [FEAT]: update address to test ht factory

--- config/hyperevm/chain.ts
+++ config/hyperevm/chain.ts
@@ -1,6 +1,6 @@
 import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts';
 
-export const FACTORY_ADDRESS = Address.fromString('0xB1c0fa0B789320044A6F623cFe5eBda9562602E3');
+export const FACTORY_ADDRESS = Address.fromString('0x3520773ebe1cf68f23e2065abd5dcc1589647f50');
 
 export const REFERENCE_TOKEN = '0x5555555555555555555555555555555555555555'; // WHYPE
 export const STABLE_TOKEN_POOL = '0x337b56d87a6185cd46af3ac2cdf03cbc37070c30'; // USDT0/WHYPE
@@ -18,16 +18,12 @@ export const ROLL_DELETE_MINUTE_LIMITER = BigInt.fromI32(1000);
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
-export const WHITELIST_TOKENS: string[] = [
-  '0x5555555555555555555555555555555555555555',
-  '0xb8ce59fc3717ada4c02eadf9682a9e934f625ebb'
-];
-
+export const WHITELIST_TOKENS: string[] = ['0x5555555555555555555555555555555555555555','0xb8ce59fc3717ada4c02eadf9682a9e934f625ebb'];
 export const STABLE_COINS: string[] = ['0xb8ce59fc3717ada4c02eadf9682a9e934f625ebb'];
 
-export const SKIP_POOLS: string[] = ['0x337b56d87a6185cd46af3ac2cdf03cbc37070c30'];
-
+export const SKIP_POOLS: string[] = [];
 export const POOL_MAPINGS: Array<Address[]> = [];
+export const STATIC_TOKEN_DEFINITIONS: TokenDefinition[] = [];
 
 export class TokenDefinition {
   address: Address;
@@ -36,4 +32,3 @@ export class TokenDefinition {
   decimals: BigInt;
 }
 
-export const STATIC_TOKEN_DEFINITIONS: TokenDefinition[] = [];
